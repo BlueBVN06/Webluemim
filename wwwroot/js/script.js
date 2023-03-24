67885eca760019c1db2f120c9d201cacf77fc644
BlueBVN06 <blueboymcgamers6578@gmail.com>
2023-03-24 03:28:47 +0000

    Add project files.

---
+++ wwwroot/js/script.js
@@ -0,0 +1,27 @@
+﻿const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "https://www.mediafire.com/file/nrc8p4jxeajqjhk/Minecraft_v1.19.63.01_64bit.apk/file";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+        }
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again</span>`;
+        }, 3000);
+    }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);
