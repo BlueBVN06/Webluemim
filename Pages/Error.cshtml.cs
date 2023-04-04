f42b1a3d9345b871d31e573aad5b8f1b02b4c53b
BlueBVN06 <blueboymcgamers6578@gmail.com>
2023-04-04 07:28:05 +0000

    up

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -11,7 +11,7 @@ namespace aspnet_core_dotnet_core.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
